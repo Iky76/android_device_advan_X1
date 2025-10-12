339c856b49a11741e9542cd5e6a151389f109ef1
Mashopy <eliasgheeraert@gmail.com>
2025-10-12 16:17:01 +0000

    X1: Configure kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,7 @@
 #
 
 DEVICE_PATH := device/advan/ADVAN_X1
+KERNEL_PATH := $(DEVICE_PATH)-kernel
 
 # A/B
 AB_OTA_UPDATER := true
@@ -49,18 +50,53 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 480
 
-# Kernel
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := ADVAN_X1_defconfig
-TARGET_KERNEL_SOURCE := kernel/advan/ADVAN_X1
 
-# Kernel - prebuilt
+# Kernel
+BOARD_KERNEL_BASE         := 0x3fff8000
+BOARD_KERNEL_OFFSET       := 0x00008000
+BOARD_KERNEL_PAGESIZE     := 4096
+BOARD_RAMDISK_OFFSET      := 0x26f08000
+BOARD_KERNEL_TAGS_OFFSET  := 0x07c88000
+BOARD_DTB_OFFSET          := 0x07c88000
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_RAMDISK_USE_LZ4 := true
+
+BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
+
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+
+BOARD_MKBOOTIMG_ARGS += \
+    --base $(BOARD_KERNEL_BASE) \
+    --dtb_offset $(BOARD_DTB_OFFSET) \
+    --header_version $(BOARD_BOOT_HEADER_VERSION) \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --pagesize $(BOARD_KERNEL_PAGESIZE) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
+    --board ""
+
+TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/$(BOARD_KERNEL_IMAGE_NAME)
 TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
-endif
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
+BOARD_MKBOOTIMG_ARGS += --dtb $(KERNEL_PATH)/dtb/mt6789.dtb
+
+# Kernel modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load.vendor_ramdisk))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/modules_ramdisk/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
+
+# Also add recovery modules to vendor ramdisk
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load.recovery))
+RECOVERY_MODULES := $(addprefix $(KERNEL_PATH)/modules_ramdisk/, $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD))
+
+# Prevent duplicated entries (to solve duplicated build rules problem)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(sort $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES) $(RECOVERY_MODULES))
+
+# Vendor modules (installed to vendor_dlkm)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/modules_dlkm/*.ko)
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
