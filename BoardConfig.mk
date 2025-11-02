2b65c2ef84ea007488fbca358c5ea62bb872a086
SzuWei Lin <szuweilin@google.com>
2025-11-02 00:34:43 +0000

    X1: Support dynamic 64-bit drmserver 64-bit GSI requires support both 64_32-bit and 64-bit devices at the same time. Add a new variable TARGET_DYNAMIC_64_32_DRMSERVER to put drmserver_dynamic.rc into GSI. Which starts 64-bit drmserver if ro.zygote is zygote64. Otherwise, it keeps the original behavior to start 32-bit drmserver.
    Bug: 282603373
    Test: make gsi_arm64-user; Check system/etc/init
    Test: make gsi_arm-user; Check systen/etc/init
    Change-Id: I874cee00a066086f565e044549ad40c6ab5d62b0
    Merged-In: I874cee00a066086f565e044549ad40c6ab5d62b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,11 +19,13 @@ AB_OTA_PARTITIONS += \
     boot
 BOARD_USES_RECOVERY_AS_BOOT := true
 
-# Include 64-bit drmserver to support 64-bit only devices
-TARGET_DYNAMIC_64_32_DRMSERVER := true
-
 # Enable 64-bit for non-zygote.
 ZYGOTE_FORCE_64 := true
+# Force any prefer32 targets to be compiled as 64 bit.
+IGNORE_PREFER32_ON_DEVICE := true
+
+# Include 64-bit drmserver to support 64-bit only devices
+TARGET_DYNAMIC_64_32_DRMSERVER := true
 
 # Architecture
 TARGET_ARCH := arm64
